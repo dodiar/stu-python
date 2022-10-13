18b197d9e030d2849db930bd6a1e832bec90d8a7
dodiar <dodiakbar.tgr@gmail.com>
2022-10-13 16:45:30 +0700

    Fundamental Python

--- fundamental1-konstruksi-dasar.py
+++ fundamental1-konstruksi-dasar.py
@@ -6,7 +6,7 @@ print('Tanggal 13 Oktober 2022')
 print('*' * 10)
 
 # Percabangan : eksekusi terpilih
-ingin_cepat = False
+ingin_cepat = True
 if ingin_cepat:
     print('jalan lurus aja ya!')
 else:
@@ -17,4 +17,3 @@ jumlah_anak=4
 
 for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
     print(f'Halo anak ke #{index_anak}')
-
